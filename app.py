8c9efd48d8525fbab593ff09026c4a52e041f69a
Maryam Ahmadi <43885330+Maryam-Ah@users.noreply.github.com>
2020-08-13 11:44:53 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from PIL import Image
 from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
 import joblib 
 import pickle
-
+from sklearn.metrics import mean_squared_error 
+from sklearn.metrics import mean_absolute_error
 
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import RandomizedSearchCV
@@ -19,22 +23,32 @@ from sklearn.model_selection import GridSearchCV
 import json
 import logging
 import os
-import pickle
-import numpy as np
-import pandas as pd
 
 
 
 
+
+
+from PIL import Image
+image = Image.open('insurance.jpg')
+st.sidebar.image(image,use_column_width=True)
+
+
+
+@st.cache
 def predict(model, input_df):
     predictions= model.predict(input_df)
     return predictions
 
+categorical_features = ['sex', 'smoker', 'region']
+numerical_features= ['age', 'bmi', 'children']
 
 st.title("Insurance Charges Prediction App")
 age = st.number_input('Age', min_value=1, max_value=100, value=25)
-sex = st.selectbox('Sex', ['male', 'female'])
-bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
+sex = st.radio("Sex",("male","female"),key='sex')
+# sex = st.selectbox('Sex', ['male', 'female'])
+# bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
+bmi = st.slider("Bmi", 10,50,key='bmi')
 children = st.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
 if st.checkbox('Smoker'):
     smoker = 'yes'
@@ -43,30 +57,50 @@ else:
     
 region = st.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
 input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
-input_df = pd.DataFrame([input_dict])
+input_df = pd.DataFrame([input_dict],index=np.arange(len(input_dict)),columns = categorical_features + numerical_features)
+
+
+
+
 
 
 
 add_selectbox = st.sidebar.selectbox(
     "How would you like to predict?",
-    ("Random Forest Regressior", "Linear Regressior","GradientBoosting, StandardScalerWrapper"))
+    ("Random Forest Regressior", "Linear Regressior","GradientBoosting"))
+
+
+
+
+
+
+
 
 # output=[]
 output=""
+
+    
+    
+# Load the data prepration pipline back from file
+Pkl_Filename = "data_prepration.pkl" 
+with open(Pkl_Filename, 'rb') as file:  
+    Pickled_data_prepration = pickle.load(file)
+
 # ...................................................
 if add_selectbox == 'Random Forest Regressior':
-    
+
     
      # Load the Model back from file
-    filename = 'rfr.pkl'
+    filename = 'rf.pkl'
     with open(filename, 'rb') as file:  
         model = pickle.load(file)
 
                        
                         
     if st.button("Predict"):
-             output = predict(model=model, input_df=input_df)
-             output = '$' + str(output[0])
+            data_prep = Pickled_data_prepration.transform(input_df)
+            output = predict(model=model, input_df=data_prep)
+            output = '$' + str(output[0])
 
 
     st.success('The output is : {}'.format(output))
@@ -74,15 +108,16 @@ if add_selectbox == 'Random Forest Regressior':
 # ..........................................
 if add_selectbox == 'Linear Regressior':
     
-    filename = 'lr.pkl'
+    filename = 'lrm.pkl'
     with open(filename, 'rb') as file:  
         model = pickle.load(file)
 
                       
                         
     if st.button("Predict"):
-             output = predict(model=model, input_df=input_df)
-             output = '$' + str(output[0])
+            data_prep = Pickled_data_prepration.transform(input_df)
+            output = predict(model=model, input_df=data_prep)
+            output = '$' + str(output[0])
 
 
     st.success('The output is : {}'.format(output))
@@ -90,17 +125,18 @@ if add_selectbox == 'Linear Regressior':
 
     
 # ..........................................
-if add_selectbox == 'GradientBoosting, StandardScalerWrapper':
+if add_selectbox == 'GradientBoosting':
     
-    filename = 'model.pkl'
+    filename = 'gb.pkl'
     with open(filename, 'rb') as file:  
         model = pickle.load(file)
 
                       
                         
     if st.button("Predict"):
-             output = predict(model=model, input_df=input_df)
-             output = '$' + str(output[0])
+            data_prep = Pickled_data_prepration.transform(input_df)
+            output = predict(model=model, input_df=data_prep)
+            output = '$' + str(output[0])
 
 
     st.success('The output is : {}'.format(output))
